8c257b94ad695f9b2269165a79c3794b354b2e9f
Ion Todirel <ion.todirel@gmail.com>
2023-06-09 23:51:08 -0700

    small fixes

--- tools/symbols/cvs_to_json/main.cpp
+++ tools/symbols/cvs_to_json/main.cpp
@@ -8,7 +8,7 @@ int main()
 {
     std::ofstream output_json_stream(OUTPUT_JSON_FILE);
 
-    io::CSVReader<8, io::trim_chars<' '>, io::double_quote_escape<',','\"'> > in("symbols.csv");    
+    io::CSVReader<8, io::trim_chars<' '>, io::double_quote_escape<',','\"'> > in(INPUT_JSON_FILE);    
     in.read_header(io::ignore_no_column, "index", "symbol", "value", "table", "overlayable", "destination address string", "ssid", "description");
     
     std::string json;
